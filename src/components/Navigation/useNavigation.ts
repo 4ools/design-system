133ef49ad6d2a257df1fe07825ba20897fd0d3b3
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-05-24 01:05:13 +0200

    just some tidying

--- src/components/Navigation/useNavigation.ts
+++ src/components/Navigation/useNavigation.ts
@@ -29,44 +29,41 @@ const formatNavigationData = (
       parts.shift();
 
       return parts.join('/');
-    });
+    })
+    .forEach(path => {
+      const parts = path.split('/');
+      const firstLevelName = parts[0];
+      const secondLevelName = parts[1];
 
-  // as we have the paths 'clean' now lets build
-  // our navigation object
-  paths.forEach(path => {
-    const parts = path.split('/');
-    const firstLevelName = parts[0];
-    const secondLevelName = parts[1];
-
-    const thisPage: PageNavigationItem = {
-      name: secondLevelName || firstLevelName || 'Home',
-      path,
-    };
-    // if we have a two level page
-    if (secondLevelName) {
-      // if the parent does not exist yet
-      if (!nav.some(item => item.name === firstLevelName)) {
-        nav.push({
-          name: firstLevelName,
-          // parent pages not have a path (they are just accordion items)
-          path: '',
-          // add this page as the first in the array of sub pages
-          subPages: [thisPage],
-        });
+      const thisPage: PageNavigationItem = {
+        name: secondLevelName || firstLevelName || 'Home',
+        path,
+      };
+      // if we have a two level page
+      if (secondLevelName) {
+        // if the parent does not exist yet
+        if (!nav.some(item => item.name === firstLevelName)) {
+          nav.push({
+            name: firstLevelName,
+            // parent pages not have a path (they are just accordion items)
+            path: '',
+            // add this page as the first in the array of sub pages
+            subPages: [thisPage],
+          });
+        } else {
+          // the parent page exists
+          nav.forEach(page => {
+            // find the parent page entry and add this sub page as one of the children
+            if (page.name === firstLevelName) {
+              page.subPages.push(thisPage);
+            }
+          });
+        }
       } else {
-        // the parent page exists
-        nav.forEach(page => {
-          // find the parent page entry and add this sub page as one of the children
-          if (page.name === firstLevelName) {
-            page.subPages.push(thisPage);
-          }
-        });
+        // not a sub page, just add it to the nav
+        nav.push(thisPage);
       }
-    } else {
-      // not a sub page, just add it to the nav
-      nav.push(thisPage);
-    }
-  });
+    });
 
   return nav;
 };
