2b0b0ecc274752761ba381d9d4fb23c7c2e5156b
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-05-24 23:11:21 +0200

    now we can generate the typings in the components module

--- packages/design-system-components/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.themes = exports.Grid = exports.Typography = void 0;
-var themes_1 = require("./themes");
-exports.themes = themes_1["default"];
-var Grid_1 = require("./components/Grid");
-exports.Grid = Grid_1["default"];
-var Typography_1 = require("./components/Typography");
-exports.Typography = Typography_1["default"];
